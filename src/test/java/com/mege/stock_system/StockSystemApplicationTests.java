bb0b9576b1ec96ba0e4a893d9a0a96053f26217a
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-12-17 09:04:34 -0300

    :tada: init: Create Project

---
+++ src/test/java/com/mege/stock_system/StockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mege.stock_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
